1e8e53f1e3746acf890a842a37fc03dd4912ada4
Albano Calamara Gonzalez <calamara43@gmail.com>
2022-05-12 12:02:49 -0300

    modificamos el archivo

--- piton/prueba.py
+++ piton/prueba.py
@@ -5,3 +5,12 @@ horas = int(input("Digite la cantidad de horas: "))
 pago= int(input("Valor de la hora de trabajo: "))
 total= int(horas*pago)
 print(f"Usted a trabajado {horas} horas, a un valor de {pago}, cobrara un total de {total} pesos.")
+print(" ")
+if total >= 1000:
+    print("Usted esta trabando bien ")
+elif total >= 1500:
+    print("Usted esta trabajando muy bien")
+elif total <= 999 and total >= 500:
+    print ("Usted no sabe trabajar")    
+else:
+    print("Usted es muy vago")
